057c5e7d3ee4f4eb1389ecc6f3caf66e3cb3751a
sbalbarrey <santiagobalbarrey@gmail.com>
2024-11-15 11:39:02 -0300

    entity publications

---
+++ src/modules/publications/entities/publication.entity.ts
@@ -0,0 +1,6 @@
+import { Entity } from 'typeorm';
+
+@Entity({
+  name: 'publications',
+})
+export default class Publication {}
--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,50 +1,49 @@
-import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({
-    name: "users"
+  name: 'users',
 })
 export default class User {
+  @PrimaryGeneratedColumn('uuid') //depende de como se origine la id se usa PrimaryColumn
+  id: string;
 
-    @PrimaryGeneratedColumn('uuid') //depende de como se origine la id se usa PrimaryColumn
-    id: string
+  // @PrimaryColumn()
+  // id: string
 
-    // @PrimaryColumn() 
-    // id: string
+  @Column()
+  name: string;
 
-    @Column()
-    name: string
+  @Column()
+  age: number;
 
-    @Column()
-    age: number
+  @Column()
+  gender: string;
 
-    @Column()
-    gender: string
+  @Column({
+    type: 'enum',
+    enum: ['searching', 'offering'],
+    default: 'offering',
+  })
+  role: string;
 
-    @Column({
-        type: "enum",
-        enum: ["searching", "offering"],
-        default: "offering"
-    })
-    role: string
+  @Column({
+    type: 'text',
+    array: true,
+    nullable: true,
+  })
+  preferences: string[]; //puede ser un enum con strings acordadas (cambiar decorador)
 
-    @Column({
-        type: 'text',
-        array: true,
-        nullable: true
-    })
-    preferences: string[] //puede ser un enum con strings acordadas (cambiar decorador)
+  @Column({
+    type: 'text',
+    array: true,
+    nullable: true,
+  })
+  travelHistory: string[];
 
-    @Column({
-        type: 'text',
-        array: true,
-        nullable: true
-    })
-    travelHistory: string[]
-
-    @Column({
-        type: 'text',
-        array: true,
-        nullable: true
-    })
-    favorites: string[] //array de festivales cuando este creado (cambiar decorador)
+  @Column({
+    type: 'text',
+    array: true,
+    nullable: true,
+  })
+  favorites: string[]; //array de festivales cuando este creado (cambiar decorador)
 }
