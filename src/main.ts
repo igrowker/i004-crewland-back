41923673b8ac0f5e8439f40c9514380bc346a868
sbalbarrey <santiagobalbarrey@gmail.com>
2024-11-15 12:37:57 -0300

    importando entity

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import dotenvOptions from './config/dotenv.config';
 import * as bodyParser from 'body-parser';
-import { WinstonLoggerService } from './middleware/logger/logger.middleware'; // Asegúrate de que la ruta sea correcta
+// import { WinstonLoggerService } from './middleware/logger/logger.middleware'; // Asegúrate de que la ruta sea correcta
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const logger = app.get(WinstonLoggerService);
+  // const logger = app.get(WinstonLoggerService);
 
-  app.useLogger(logger);
+  // app.useLogger(logger);
   app.enableCors();
   app.use(bodyParser.json());
 
