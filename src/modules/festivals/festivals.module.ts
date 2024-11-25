662c48c976c9676c8a1921eb3ea57a2956de0452
Eduardolans <141261251+Eduardolans@users.noreply.github.com>
2024-11-25 18:26:57 +0100

    update filename used in controller spec; simplify code in module

--- src/modules/festivals/festivals.module.ts
+++ src/modules/festivals/festivals.module.ts
@@ -1,38 +1,40 @@
-// import { Module } from '@nestjs/common';
-// import { FestivalsService } from './festivals.service';
-// import { FestivalsController } from './festivals.controller';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { Festivals } from './entities/festival.entity';
-
-// @Module({
-//   imports: [TypeOrmModule.forFeature([Festivals])],
-//   controllers: [FestivalsController],
-//   providers: [FestivalsService],
-// })
-// export class FestivalsModule {}
-
 import { Module } from '@nestjs/common';
 import { FestivalsService } from './festivals.service';
 import { FestivalsController } from './festivals.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Festivals } from './entities/festival.entity';
-import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Festivals]),
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        secret: configService.get('JWT_SECRET'),
-        signOptions: { expiresIn: '8h' },
-      }),
-      inject: [ConfigService],
-    }),
-    ConfigModule, // This ensures ConfigService is available in this module
-  ],
+  imports: [TypeOrmModule.forFeature([Festivals]), ConfigModule],
   controllers: [FestivalsController],
   providers: [FestivalsService],
+  exports: [TypeOrmModule],
 })
 export class FestivalsModule {}
+
+// import { Module } from '@nestjs/common';
+// import { FestivalsService } from './festivals.service';
+// import { FestivalsController } from './festivals.controller';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { Festivals } from './entities/festival.entity';
+// import { JwtModule } from '@nestjs/jwt';
+// import { ConfigModule, ConfigService } from '@nestjs/config';
+
+// @Module({
+//   imports: [
+//     TypeOrmModule.forFeature([Festivals]),
+//     JwtModule.registerAsync({
+//       imports: [ConfigModule],
+//       useFactory: async (configService: ConfigService) => ({
+//         secret: configService.get('JWT_SECRET'),
+//         signOptions: { expiresIn: '8h' },
+//       }),
+//       inject: [ConfigService],
+//     }),
+//     ConfigModule, // This ensures ConfigService is available in this module
+//   ],
+//   controllers: [FestivalsController],
+//   providers: [FestivalsService],
+// })
+// export class FestivalsModule { }
