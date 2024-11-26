8e06136c8906fef25c6b68504c7f78454bcd9603
sbalbarrey <santiagobalbarrey@gmail.com>
2024-11-26 15:43:34 -0300

    implementacion de chat 1 a 1 con socket

--- src/modules/chat/chat.module.ts
+++ src/modules/chat/chat.module.ts
@@ -1,13 +1,26 @@
 // src/chat/chat.module.ts
+// import { Module } from '@nestjs/common';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { ChatService } from './chat.service';
+// import { ChatGateway } from './chat.gateway';
+// import { Message } from './entities/chat.entity';
+
+// @Module({
+//   imports: [TypeOrmModule.forFeature([Message])],
+//   providers: [ChatGateway, ChatService],
+// })
+// export class ChatModule {}
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChatService } from './chat.service';
 import { ChatGateway } from './chat.gateway';
+import { ChatController } from './chat.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Message } from './entities/chat.entity';
+import { Message } from '../chat/entities/message.entity';
+import { Room } from '../chat/entities/room.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Message])],
-  providers: [ChatGateway, ChatService],
+  imports: [TypeOrmModule.forFeature([Message, Room])],
+  providers: [ChatService, ChatGateway],
+  controllers: [ChatController],
 })
 export class ChatModule {}
