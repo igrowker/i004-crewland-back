e9534b4d6aa78c67ed810e916f0294fc856daffd
sbalbarrey <santiagobalbarrey@gmail.com>
2024-11-26 17:04:55 -0300

    endpoints chat

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -1,14 +1,52 @@
+// import { DataSource, DataSourceOptions } from 'typeorm';
+// import { Message } from '../modules/chat/entities/chat.entity';
+// import { Publication } from '../modules/publications/entities/publication.entity';
+// import { Reservations } from 'src/modules/reservations/entities/reservation.entity';
+// import { User } from '../modules/users/entities/user.entity';
+// import { Auth } from 'src/modules/auth/entities/auth.entity';
+// import { Festivals } from '../modules/festivals/entities/festival.entity';
+// import { VerificationCodeEmail } from '../modules/verification/entities/email-verification.entity';
+
+// import dotEnvOptions from './dotenv.config';
+// import { VerificationCodeSms } from 'src/modules/verification/entities/sms-verification.entity';
+
+// export const dataSourceOptions: DataSourceOptions = {
+//   type: 'postgres',
+//   port: parseInt(dotEnvOptions.DB_PORT || '5432', 10),
+//   host: dotEnvOptions.DB_HOST,
+//   username: dotEnvOptions.DB_USERNAME,
+//   password: dotEnvOptions.DB_PASSWORD,
+//   database: dotEnvOptions.DB_NAME,
+//   dropSchema: dotEnvOptions.DB_MIGRATE_DATA === 'false',
+//   synchronize: true,
+//   logging: false,
+//   entities: [
+//     Festivals,
+//     Publication,
+//     Message,
+//     User,
+//     Reservations,
+//     Auth,
+//     VerificationCodeSms,
+//     VerificationCodeEmail,
+//   ],
+//   subscribers: [],
+//   migrations: [],
+// };
+
+// export const AppDataSource = new DataSource(dataSourceOptions);
 import { DataSource, DataSourceOptions } from 'typeorm';
-import { Message } from '../modules/chat/entities/chat.entity';
 import { Publication } from '../modules/publications/entities/publication.entity';
 import { Reservations } from 'src/modules/reservations/entities/reservation.entity';
 import { User } from '../modules/users/entities/user.entity';
 import { Auth } from 'src/modules/auth/entities/auth.entity';
 import { Festivals } from '../modules/festivals/entities/festival.entity';
-import { VerificationCodeEmail } from '../modules/verification/entities/email-verification.entity';
+import { VerificationCodeSms } from '../modules/verification/entities/sms-verification.entity'; //verificacion
+import { VerificationCodeEmail } from '../modules/verification/entities/email-verification.entity'; //verificacion
 
 import dotEnvOptions from './dotenv.config';
-import { VerificationCodeSms } from 'src/modules/verification/entities/sms-verification.entity';
+import { Message } from 'src/modules/chat/entities/message.entity';
+import { Room } from '../modules/chat/entities/room.entity';
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
@@ -23,12 +61,13 @@ export const dataSourceOptions: DataSourceOptions = {
   entities: [
     Festivals,
     Publication,
-    Message,
     User,
     Reservations,
     Auth,
     VerificationCodeSms,
     VerificationCodeEmail,
+    Message,
+    Room,
   ],
   subscribers: [],
   migrations: [],
--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,6 +1,61 @@
-import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+// import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+// import { Role, Gender } from 'src/shared/utils/enum';
+// import { Message } from 'src/modules/chat/entities/chat.entity';
+
+// @Entity('users')
+// export class User {
+//   @PrimaryGeneratedColumn('uuid')
+//   id: string;
+
+//   @Column({ type: 'varchar', nullable: false })
+//   password: string;
+
+//   @Column({ type: 'varchar', nullable: false })
+//   name: string;
+
+//   @Column({ type: 'varchar', unique: true, nullable: false })
+//   username: string;
+
+//   @Column({ type: 'varchar', unique: true, nullable: false })
+//   email: string;
+
+//   @Column({ type: 'varchar', nullable: false })
+//   age: string;
+
+//   @Column({ type: 'varchar', unique: true, nullable: false })
+//   tel: string;
+
+//   @Column({
+//     type: 'enum',
+//     enum: Gender,
+//   })
+//   gender: string;
+
+//   @Column({
+//     type: 'enum',
+//     enum: Role,
+//     default: Role.User,
+//   })
+//   role: string;
+
+//   @Column({ type: 'simple-array', nullable: true })
+//   preferences: string[];
+
+//   @Column({ type: 'simple-array', nullable: true })
+//   travelHistory: string[];
+
+//   @Column({ type: 'simple-array', nullable: true })
+//   favorites: string[];
+
+//   @ManyToMany(() => Message, (chat) => chat.senderId)
+//   chats: Message[];
+// }
+
+// agregar imagen con cloudinary
+// descripcion
+// location ( string)
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { Role, Gender } from 'src/shared/utils/enum';
-import { Message } from 'src/modules/chat/entities/chat.entity';
 
 @Entity('users')
 export class User {
@@ -46,11 +101,8 @@ export class User {
 
   @Column({ type: 'simple-array', nullable: true })
   favorites: string[];
-
-  @ManyToMany(() => Message, (chat) => chat.senderId)
-  chats: Message[];
 }
 
 // agregar imagen con cloudinary
 // descripcion
-// location ( string)
+// location ( string )
