f541552d8e89607834becbc150a9e19b98e0903a
AgusBirman <agusbirman@gmail.com>
2024-11-29 01:10:45 +0100

    CREATED GUARDS user validator (general, publication, reservations and users)

---
+++ src/shared/guards/user-validator/user-validator-publication.guard.ts
@@ -0,0 +1,31 @@
+import {
+    Injectable,
+    CanActivate,
+    ExecutionContext,
+    ForbiddenException,
+} from '@nestjs/common';
+import { PublicationsService } from '../../../modules/publications/publications.service';
+
+@Injectable()
+export class PublicationValidationUser implements CanActivate {
+    constructor(private readonly publicationService: PublicationsService) { }
+
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const request = context.switchToHttp().getRequest();
+        const user = request.user;
+        const publicationId = request.params.id;
+
+        const publication = await this.publicationService.findOne(publicationId);
+        if (!publication) {
+            throw new ForbiddenException('Publicación no encontrada');
+        }
+
+        if (publication.userId !== user.sub) {
+            throw new ForbiddenException(
+                'No tienes permiso para acceder a esta publicación.',
+            );
+        }
+
+        return true;
+    }
+}
