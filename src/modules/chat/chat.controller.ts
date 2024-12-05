43ecaa7fd691b4e6ed57f0bd28665007383c83fb
Ulises <uliisesrodriguez809@gmail.com>
2024-12-05 09:55:26 -0300

    cambios en chat

--- src/modules/chat/chat.controller.ts
+++ src/modules/chat/chat.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 import { ChatService } from './chat.service';
 
@@ -8,7 +9,7 @@ export class ChatController {
   // Crear o buscar una sala
   @Post('rooms')
   async createRoom(@Body('roomName') roomName: string) {
-    return this.chatService.getOrCreateRoom(roomName);
+    // return this.chatService.getOrCreateRoom(roomName);
   }
 
   // Enviar un mensaje a una sala
@@ -18,12 +19,12 @@ export class ChatController {
     @Body('senderId') senderId: string,
     @Body('content') content: string,
   ) {
-    return this.chatService.saveMessage(senderId, content, roomName);
+    // return this.chatService.saveMessage(senderId, content, roomName);
   }
 
   // Obtener todos los mensajes de una sala
   @Get('rooms/:roomName/messages')
   async getMessages(@Param('roomName') roomName: string) {
-    return this.chatService.getMessages(roomName);
+    // return this.chatService.getMessages(roomName);
   }
 }
